a4b0e6c5604e39f13bc680bca358e5dfe2342587
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-18 17:58:45 +0100

    added import path with adult text component
    co-authored-by: Maria <maria.louisa.castro@gmail.com>

--- pages/adult-onboarding3.js
+++ pages/adult-onboarding3.js
@@ -2,10 +2,12 @@ import Link from "next/link";
 import styled from "styled-components";
 import AdultsBack from "../components/AdultsBack";
 import AdultsNext from "../components/AdultsNext";
+import AdultText from "../components/Styled-Components/AdultText";
 
 export default function Adult() {
   return (
     <>
+      <AdultText>Choose a daily goal for the learner.</AdultText>
       <AdultsNext page={3} />
       <AdultsBack page={3} />
     </>
