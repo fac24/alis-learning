530cff64c015155891677e2a28dfbbd640c28857
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-19 17:03:06 +0100

    finished learner name page and styled with components
    co-authored-by: Joe <joe-dev-public@users.noreply.github.com>

--- pages/learner-onboarding/name.js
+++ pages/learner-onboarding/name.js
@@ -1,12 +1,45 @@
-import LearnerOnboardingLayout from "../../components/LearnerOnboardingLayout";
+// 1. alis-learning title above.
+// 2. progress bar component will go after
+// 3. what is your name speech click icon. Also include text prompting the learner.
+// 4. form with will include input area for learner to type name.
+// 5. orange next button on bottom center.
+// 6. back button on bottom left.
 
-export default function LearnerAvatarSelection() {
+import styled from "styled-components";
+import LearnerOnboardingLayout from "../../components/LearnerOnboardingLayout";
+import LearnerOnboardingName from "../../components/LearnerOnboardingName";
+
+export default function LearnerChoiceName() {
   return (
     <LearnerOnboardingLayout
       ttsTitle="What is your name?"
       stepNumber="1"
       nextStep="font-selection"
       previousStep="avatar-selection"
-    />
+    >
+      {/* <LearnerOnboardingName /> */}
+      <StyledForm>
+        <label for="learnerName"></label>
+        <StyledInput type="text" name="learnerName" />
+      </StyledForm>
+      {/* Content goes here */}
+    </LearnerOnboardingLayout>
   );
 }
+
+const StyledForm = styled.form`
+  display: flex;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const StyledInput = styled.input`
+  text-align: center;
+  font-size: 2rem;
+  padding: 2rem;
+  color: black;
+  border-radius: 2.813rem;
+  border: 0.313rem solid black;
+  box-shadow: inset 0 0 0.625rem rgba(0, 0, 0, 0.5);
+`;
