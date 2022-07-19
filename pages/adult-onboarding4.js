ce32fa474fc6d49e2951c9cb1560364fa3970072
Maria <maria.louisa.castro@gmail.com>
2022-07-19 10:46:19 +0100

    Added form to select goal
    Co-authored-by: Peter James Salter <PJSalter@users.noreply.github.com>

--- pages/adult-onboarding4.js
+++ pages/adult-onboarding4.js
@@ -1,12 +1,31 @@
-import Link from "next/link";
-import styled from "styled-components";
 import AdultsBack from "../components/AdultsBack";
 import AdultsNext from "../components/AdultsNext";
+import AdultText from "../components/Styled-Components/AdultText";
 
 export default function Adult() {
+  const getGoal = (e) => {
+    e.preventDefault();
+    // picking up the age.
+    // console.log(e.target.age.value);
+    let learnersGoal = e.target.time.value;
+    console.log(learnersGoal);
+    // here we will handle saving the users age to local storage.
+  };
+
   return (
     <>
-      <AdultsNext page={4} />
+      <AdultText>Choose a daily goal for the learner.</AdultText>
+      <form onSubmit={getGoal}>
+        <label>
+          <select type="select" name="time">
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+          </select>
+          minutes
+        </label>
+        <AdultsNext page={4} />
+      </form>
       <AdultsBack page={4} />
     </>
   );
