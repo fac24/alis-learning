011259ffccb95e09abe953a4f41aac3a2efa22b1
Maria <maria.louisa.castro@gmail.com>
2022-07-19 10:45:34 +0100

    Removed conments and green styling
    Co-authored-by: Peter James Salter <PJSalter@users.noreply.github.com>

--- components/Layout.js
+++ components/Layout.js
@@ -23,31 +23,5 @@ export default function Layout({ children }) {
 }
 
 const StyledTitle = styled.h1`
-  color: green;
+  color: black;
 `;
-
-// import Nav from "./Nav";
-// // import PaintboxSparkleLogoImage from "../styled-components/PaintboxSparkleLogoImage";
-// import Image from "next/image";
-// import logo from "../../Public/paintbox-logo.png";
-// import styled from "styled-components";
-
-// function Layout(props) {
-//   return (
-//     <div>
-//       <Header>
-//         <Image src={logo} alt="paintbox-logo" height={250} width={300} />
-//       </Header>
-//       <main>{props.children}</main>
-//     </div>
-//   );
-// }
-
-// export default Layout;
-
-// const Header = styled.header`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   flex-direction: column;
-// `;
