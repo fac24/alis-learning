611d3245cc1241413231862f5cd10384cf1dc596
joe <joe.dev.public@gmail.com>
2022-07-20 14:18:49 +0100

    Avatar selection progress

--- pages/learner-onboarding/avatar-selection.js
+++ pages/learner-onboarding/avatar-selection.js
@@ -1,7 +1,20 @@
 import LearnerOnboardingLayout from "../../components/LearnerOnboardingLayout";
-//import Caterpillar from "../../public/avatars/caterpillar.svg";
+import styled from "styled-components";
 
 export default function LearnerAvatarSelection() {
+  const avatars = {
+    butterfly: "/avatars/butterfly.svg",
+    caterpillar: "/avatars/caterpillar.svg",
+    ladybird: "/avatars/ladybird.svg",
+    crab: "/avatars/crab.svg",
+    fish: "/avatars/fish.svg",
+    penguin: "/avatars/penguin.svg",
+    snail: "/avatars/snail.svg",
+    warhol: "/avatars/warhol.svg",
+  };
+
+  console.log(Object.entries(avatars).map((element) => element[1]));
+
   return (
     <LearnerOnboardingLayout
       ttsTitle="Pick your avatar"
@@ -10,7 +23,43 @@ export default function LearnerAvatarSelection() {
       // we need to change the previous step!
       previousStep=""
     >
-      Avatar selection code goes here
+      <StyledUnorderedList>
+        {/*
+          Object.entries(avatars).map(([avatar_name, avatar_file]) => (
+         
+<StyledListItem>
+            <img src={avatar_file} alt={avatar_name} />
+          </StyledListItem>
+
+));
+        */}
+      </StyledUnorderedList>
     </LearnerOnboardingLayout>
   );
 }
+
+const StyledUnorderedList = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 4rem auto 4rem auto;
+  max-width: 44rem;
+  padding: 0;
+`;
+
+const StyledListItem = styled.li`
+  border: 5px solid #ccc;
+  border-radius: 1rem;
+  cursor: pointer;
+  margin: 0.5rem;
+  padding: 0.5rem;
+  width: 10rem;
+
+  &:hover {
+    border-color: #666;
+  }
+
+  &.selected {
+    border-color: black;
+  }
+`;
