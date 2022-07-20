214991c2f26320382498581b2b27e03299e1a7a6
Maria <maria.louisa.castro@gmail.com>
2022-07-20 16:32:37 +0100

    Correct path for button

--- pages/learner-onboarding/onboarding-done.js
+++ pages/learner-onboarding/onboarding-done.js
@@ -4,10 +4,9 @@ export default function OnboardingDone() {
   return (
     <LearnerOnboardingLayout
       ttsTitle="Well done! Now you're ready to play a game."
-      // we need to change this next step once we have the map page!
       stepNumber="5"
       previousStep="background-selection"
-      nextStep="/"
+      nextStep="child-landing"
       completed="true"
     />
   );
