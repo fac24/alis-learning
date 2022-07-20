56df2e29f57ff8b9cf58d03f906515b7abb8ac28
joe <joe.dev.public@gmail.com>
2022-07-20 15:33:14 +0100

    Add avatar.js data object

---
+++ data/avatars.js
@@ -0,0 +1,10 @@
+export default {
+  butterfly: "/avatars/butterfly.svg",
+  caterpillar: "/avatars/caterpillar.svg",
+  ladybird: "/avatars/ladybird.svg",
+  crab: "/avatars/crab.svg",
+  fish: "/avatars/fish.svg",
+  penguin: "/avatars/penguin.svg",
+  snail: "/avatars/snail.svg",
+  warhol: "/avatars/warhol.svg",
+};
