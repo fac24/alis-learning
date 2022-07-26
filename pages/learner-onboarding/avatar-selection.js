c195aa3e949140e77bcbdd9befc709d0b52ea0ec
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-07-26 21:40:40 +0100

    changed size of avatar

--- pages/learner-onboarding/avatar-selection.js
+++ pages/learner-onboarding/avatar-selection.js
@@ -85,12 +85,12 @@ const StyledListItem = styled.li`
   }
 `;
 
-const AVATAR_SIZE = "48px";
+const AVATAR_SIZE = "65px";
 // const DURATION = "0.5s";
 const DURATION = "1s";
 const SHADOW_HEIGHT = `${Math.round(48 / 5)}px`;
 const DROP_HEIGHT = "200px";
-const OFFSET = "100px";
+const OFFSET = "0px";
 
 const bounce = keyframes`
   from {
@@ -113,7 +113,7 @@ const grow = keyframes`
 const StyledImage = styled.img`
   position: absolute;
   top: ${OFFSET};
-  left: calc(50% - ${AVATAR_SIZE} / 2);
+  left: calc(25% - ${AVATAR_SIZE} / 2);
   animation-name: ${bounce};
   animation-duration: ${DURATION};
   animation-direction: alternate;
@@ -133,7 +133,7 @@ const Shadow = styled.div`
   top: calc(
     ${OFFSET} + ${DROP_HEIGHT} + ${AVATAR_SIZE} - 1.5 * ${SHADOW_HEIGHT}
   );
-  left: calc(50% - ${AVATAR_SIZE} / 2);
+  left: calc(25% - ${AVATAR_SIZE} / 2);
   animation-name: ${grow};
   animation-duration: ${DURATION};
   animation-direction: alternate;
