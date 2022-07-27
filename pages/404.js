a2f1bb0b707523df2f17ae3c20f86aa4803745ae
vasystus <artgalla8@gmail.com>
2022-07-27 16:47:23 +0100

    404 fixed

--- pages/404.js
+++ pages/404.js
@@ -1,18 +1,25 @@
 import PhonemeLayout from "../components/PhonemeLayout";
 import styled from "styled-components";
 import Star from "../public/decorations/star.svg";
+import Rainbow from "../public/decorations/Rainbow.svg";
 import Image from "next/image";
 
 export default function ErrorPage({ stars, setStars }) {
   return (
     <>
-      <PhonemeLayout stars={stars}>
-        <Oops>Oops, nothing to see here yet!</Oops>
+      <PhonemeLayout
+        stars={stars}
+        firstText={"Oops, nothing to see here yet!"}
+        secondText={"Please come back later"}
+      >
+        <RainbowDiv>
+          <Image src={Rainbow} alt={"rainbow"} width={200} height={300} />
+        </RainbowDiv>
       </PhonemeLayout>
     </>
   );
 }
 
-const Oops = styled.h2`
+const RainbowDiv = styled.div`
   text-align: center;
 `;
