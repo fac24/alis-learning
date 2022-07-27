f8a5564548389b92903519122bc72ae374c4ec4d
joe <joe.dev.public@gmail.com>
2022-07-27 12:01:43 +0100

    Add lang attribute to html element

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+// https://nextjs.org/docs/advanced-features/custom-document
+
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head />
       <body>
         <Main />
