27ac4077b042558da91e87cf2fa1c39b35f6ae06
Maria <maria.louisa.castro@gmail.com>
2022-07-27 17:09:24 +0100

    Adjust postitioning of next button

--- components/Styled-Components/AdultNext.js
+++ components/Styled-Components/AdultNext.js
@@ -1,10 +1,5 @@
 import styled from "styled-components";
 
-// The absolute and and top/left 50% move the buttons top left corner to the center of the page
-// minus some margin on top and left to bring the whole thing back to the center
-// If you want to adjust the position change the 'margin-top' and the 'margin-left' accordingly
-// ask maria if you are confused (:
-
 const AdultNext = styled.button`
   display: flex;
   text-align: center;
@@ -12,13 +7,12 @@ const AdultNext = styled.button`
   margin-left: auto;
   margin-right: auto;
   margin-top: 10rem;
-  position: absolute;
-  bottom: 0rem;
-  width: 12rem;
+  width: 13rem;
   padding: 1.5rem;
   box-shadow: 5px 10px 8px #888888;
   color: white;
   font-size: 15px;
+  font-family: rial, Helvetica, sans-serif;
   border-radius: 20px;
   border: none;
   background-color: #4316a3;
