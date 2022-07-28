4ba8ed086560954391f7ade0976311c35bdc4148
vasystus <artgalla8@gmail.com>
2022-07-28 10:00:54 +0100

    several tests added

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,76 +5,85 @@ describe("empty spec", () => {
 });
 
 // // ✅ --- PASSING ---
-// describe("Get started route", () => {
-//   it("Visiting the Avatars Page", () => {
-//     cy.visit("/");
-//     cy.get("h1").contains("Alis Learning");
-//     cy.get("#get_started").click();
-//     cy.intercept("/learner-onboarding/avatar-selection");
-//     cy.intercept({
-//       method: "GET",
-//       url: "/avatars",
-//       hostname: "localhost",
-//     });
-//     cy.get("main").contains("snail.svg");
-//   });
-// });
+describe("Get started route", () => {
+  it("Visiting the Avatars Page", () => {
+    cy.visit("/");
+    cy.get("h1").contains("Alis Learning");
+    cy.get("#get_started").click();
+    cy.intercept("/learner-onboarding/avatar-selection");
+    cy.intercept({
+      method: "GET",
+      url: "/avatars",
+      hostname: "localhost",
+    });
+    // cy.get("main").find(".Comic");
+  });
+});
 
-// // ✅ --- PASSING ---
-// describe("Parent route", () => {
-//   it("should be navigate all pages through the parent route", () => {
-//     cy.visit("/");
-//     cy.get("h1").contains("Alis Learning");
-//     cy.get("h1").should("not.contain", "Feedback Form");
-//     cy.contains("Go to parents section").click();
-//     cy.url().should("include", "/adult-onboarding1");
-//     cy.contains("How can Alis Learning help with dyslexia?").click().end();
-//     cy.contains("Get a learner started").click();
-//     cy.url().should("include", "/adult-onboarding2");
-//     cy.contains("Welcome!");
-//     cy.contains("Next").click();
-//     cy.url().should("include", "/adult-onboarding3");
-//     cy.get("main").find("input");
-//     cy.contains("Next").click();
-//     cy.location("pathname").should("eq", "/adult-onboarding4");
-//     cy.get("main").find("form");
-//     cy.wait(0);
-//     cy.contains("Next").click();
-//     // cy.wait();
-//     cy.url().should("include", "/adult-onboarding5");
-//     cy.contains("Great");
-//     cy.go("back");
-//   });
-// });
+// ✅ --- PASSING ---
+describe("Parent route", () => {
+  it("should be navigate all pages through the parent route", () => {
+    cy.visit("/");
+    cy.get("h1").contains("Alis Learning");
+    cy.get("h1").should("not.contain", "Feedback Form");
+    cy.contains("Go to parents section").click();
+    cy.url().should("include", "/adult-onboarding1");
+    cy.contains("How can Alis Learning help with dyslexia?").click().end();
+    cy.contains("Get a learner started").click();
+    cy.url().should("include", "/adult-onboarding2");
+    cy.contains("Welcome!");
+    cy.contains("Next").click();
+    cy.url().should("include", "/adult-onboarding3");
+    cy.get("main").find("input");
+    cy.contains("Next").click();
+    cy.location("pathname").should("eq", "/adult-onboarding4");
+    cy.get("main").find("form");
+    cy.wait(0);
+    cy.contains("Next").click();
+    // cy.wait();
+    cy.url().should("include", "/adult-onboarding5");
+    cy.contains("Great");
+    cy.go("back");
+  });
+});
 
-// // ✅ --- PASSING ---
-// describe("Location", () => {
-//   it("should assert location", () => {
-//     cy.visit("http://localhost:3000/learner-onboarding/font-selection");
+// ✅ --- PASSING ---
+describe("Location", () => {
+  it("should assert location", () => {
+    cy.visit("http://localhost:3000/learner-onboarding/font-selection");
 
-//     cy.location().should((loc) => {
-//       expect(loc.host).to.eq("localhost:3000");
-//       expect(loc.hostname).to.eq("localhost");
-//       expect(loc.href).to.eq(
-//         "http://localhost:3000/learner-onboarding/font-selection"
-//       );
-//       expect(loc.origin).to.eq("http://localhost:3000");
-//       expect(loc.pathname).to.eq("/learner-onboarding/font-selection");
-//       expect(loc.port).to.eq("3000");
-//       expect(loc.protocol).to.eq("http:");
-//       expect(loc.toString()).to.eq(
-//         "http://localhost:3000/learner-onboarding/font-selection"
-//       );
-//     });
-//   });
-// });
+    cy.location().should((loc) => {
+      expect(loc.host).to.eq("localhost:3000");
+      expect(loc.hostname).to.eq("localhost");
+      expect(loc.href).to.eq(
+        "http://localhost:3000/learner-onboarding/font-selection"
+      );
+      expect(loc.origin).to.eq("http://localhost:3000");
+      expect(loc.pathname).to.eq("/learner-onboarding/font-selection");
+      expect(loc.port).to.eq("3000");
+      expect(loc.protocol).to.eq("http:");
+      expect(loc.toString()).to.eq(
+        "http://localhost:3000/learner-onboarding/font-selection"
+      );
+    });
+  });
+});
 
-
-describe("checking the 404 page", () => {
-  it("can't find the page", () => {
+// ✅ --- PASSING ---
+describe("Checking the 404 page", () => {
+  it("should redirect to 404 page if clicks fake links", () => {
     cy.visit("/child-landing");
-    cy.contains('Story Time').click();
+    cy.contains("Story Time").click();
     cy.url().should("include", "/404");
     cy.get("h1").should("not.contain", "Cinderella");
+  });
+});
+
+// ✅ --- PASSING ---
+describe("window properties", () => {
+  it("window should have a special properties", () => {
+    cy.window().should("have.property", "top");
+    cy.document().should("have.property", "charset").and("eq", "UTF-8");
+    cy.title().should("not.contain", "Reading App");
   });
 });
